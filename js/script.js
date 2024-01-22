2539fb213632a291e2e13fef2d12ce70eaa6ca3d
David Kilpatrick <Davidkilpatrick@outlook.com>
2024-01-22 20:35:49 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I Am John Doe.'
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 120;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+
+function typeText(text, typingElement, typingDelay) {
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i)
+        }, typingDelay * i)
+    }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', runTypingEffect)
